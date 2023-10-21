8d2ed9e45e937ecb776efb5c3ae2f34ca9508c96
Tyuiu.SorokinMA@outlook.com <lorik.sorokin01@mail.ru>
2023-10-22 03:59:19 +0500

    Task3

---
+++ Tyuiu.SorokinMA.Sprint3.Task3.V21.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SorokinMA.Sprint3.Task3.V21.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SorokinMA.Sprint3.Task3.V21.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ceabae3a-b21c-4c49-bd7a-692c73ecd4a9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
